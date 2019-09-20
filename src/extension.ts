0a7f53def636860c0ec03cc96ab21bd31ee2a13a
Krnik <marcin11qwerty@gmail.com>
2019-09-20 18:21:21 +0200

    premerge

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,5 @@
-'use strict';
 import { ExtensionContext } from 'vscode';
 import { Jumpy } from './jumpy/mod';
-
-// Add primary charset setting
-// Register Type command on jumpy enter
-// Deregister Type command on jumpy exit
-// Deregister commands on deactivate
-// Add settings to switch between icon and text markers
-// Jump N lines up/down on "[" or "]" keypress
-// Toggle display mode "
 
 const jumpy = new Jumpy();
 
@@ -16,7 +7,6 @@ export function activate(_context: ExtensionContext): void {
     jumpy.activate();
 }
 
-// tsling-disable
 export function deactivate(): void {
     jumpy.deactivate();
 }
